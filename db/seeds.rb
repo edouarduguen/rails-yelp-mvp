896dacd20c438b1ab7bb00ff88d50c83599b2736
edouard uguen <uguen.edouard@gmail.com>
2019-08-07 15:12:01 +0200

    La fouchette du pauvre

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: "Chinois à volonté",
+    address: "Paris",
+    category: "chinese"
+  },
+
+  {
+    name: "Paul",
+    address: "Bordeaux",
+    category: "french"
+  },
+
+  {
+    name: "Kebab frite",
+    address: "Brussel",
+    category: "belgian"
+  },
+
+  {
+    name: "Sushi",
+    address: "Tokyo",
+    category: "japanese"
+  },
+
+  {
+    name: "Macdo",
+    address: "nantes",
+    category: "french"
+  },
+
+  {
+    name: "Pizzanini",
+    address: "Rome",
+    category: "italian"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
